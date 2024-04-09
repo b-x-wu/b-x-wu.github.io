7e7fbd2d29f0aacd013fc125fe96c86e46a45b35
b-x-wu <bridgette.x.wu@gmail.com>
2024-04-08 23:44:16 -0700

    feat: add router to rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-const App = () => {
-    return (
-        <>
-            This is a React App! Modify src/index.tsx to get started.
-        </>
-    )
-}
+import Router from './Router'
+import { RouterProvider } from 'react-router-dom'
 
 const root = createRoot(document.getElementById('root')!)
-root.render(<App />)
-
+root.render(<RouterProvider router={Router} />)
