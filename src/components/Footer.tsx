6070d6f913841d35f14c20a8ea59e9dc117e130a
b-x-wu <bridgette.x.wu@gmail.com>
2024-05-05 14:39:08 -0700

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <div className='absolute bottom-0 w-full flex flex-col content-center gap-y-3 p-6 text-disabled text-sm'>
+            <div className='mx-auto w-fit'>---</div>
+            <div className='mx-auto w-fit'>
+                <span>Made with </span>
+                <span aria-labelledby='love'>♥</span>
+                <span> with an oat milk latte</span>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
