d519d4ff9fbcea7716c64759dd1594fbd0226d95
Bruce Wu <brucexwu@amazon.com>
2024-05-06 11:59:09 -0700

    style: refactored resume sections

--- src/components/resume/Experience.tsx
+++ src/components/resume/Experience.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-export interface WorkExperienceProps {
-    position: string;
-    dates: string;
+export interface ExperienceProps {
+    title: string;
+    dates?: string;
     bulletPoints: string[];
 }
 
-const WorkExperience: React.FC<WorkExperienceProps> = ({
-    position,
+const Experience: React.FC<ExperienceProps> = ({
+    title,
     dates,
     bulletPoints,
 }) => {
     return (
         <div className='flex flex-col gap-y-2'>
             <div className='flex flex-col md:flex-row md:place-content-between md:align-baseline'>
-                <div className='text-lg'>{position}</div>
-                <div className='text-sm md:text-lg'>{dates}</div>
+                <div className='text-lg'>{title}</div>
+                { dates !== undefined ? <div className='text-sm md:text-lg'>{dates}</div> : <></> }
             </div>
             <ul className='flex list-inside list-image-bullet-disabled flex-col gap-y-2 text-disabled'>
                 {bulletPoints.map((bulletPoint, bulletPointIndex) => (
@@ -26,4 +26,4 @@ const WorkExperience: React.FC<WorkExperienceProps> = ({
     );
 };
 
-export default WorkExperience;
+export default Experience;
--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -1,70 +1,87 @@
 import React from 'react';
 import { useTitle } from '../hooks';
-import WorkExperience from '../components/resume/WorkExperience';
+import ExperienceSection from '../components/resume/ExperienceSection';
 
 const Resume: React.FC = () => {
     useTitle('b-x-wu | Resume');
 
     return (
-        <div className='flex flex-col gap-y-8'>
+        <div className='flex flex-col gap-y-6'>
+            {/*Basic Info*/}
             <div className='flex flex-col gap-y-2'>
                 <div className='text-3xl font-bold'>Bridgette X. Wu</div>
                 <div className='text-disabled'>they/she</div>
             </div>
+            {/*Work Experience*/}
             <div className='flex flex-col gap-y-6'>
                 <div className='text-2xl font-bold'>Work Experience</div>
-                <div className='flex flex-col gap-y-4'>
-                    <div className='text-xl text-secondary'>Amazon</div>
-                    <div className='flex flex-col gap-y-4'>
-                        <WorkExperience
-                            position='Software Development Engineer'
-                            dates='October 2023 - Today'
-                            bulletPoints={[ 'WIP' ]}
-                        />
-                        <WorkExperience
-                            position='Software Development Intern'
-                            dates='June 2022 - August 2022'
-                            bulletPoints={[
+                <ExperienceSection
+                    header='Amazon'
+                    experiences={[
+                        {
+                            title: 'Software Development Engineer',
+                            dates: 'October 2023 - Today',
+                            bulletPoints: [ 'WIP' ],
+                        },
+                        {
+                            title: 'Software Development Intern',
+                            dates: 'June 2022 - August 2022',
+                            bulletPoints: [
                                 'Created a web app with a back end written in Java and a front-end written in React using Typescript all served with AWS CloudWatch',
                                 'Delivered a 30-minute demo presentation to 20 clients and composed a three-page wiki article detailing the service for future developers',
-                            ]}
-                        />
-                    </div>
-                </div>
-                <div className='flex flex-col gap-y-4'>
-                    <div className='text-xl text-secondary'>Bit Project</div>
-                    <div className='flex flex-col gap-y-4'>
-                        <WorkExperience
-                            position='Program Director'
-                            dates='February 2021 - July 2021'
-                            bulletPoints={[
+                            ],
+                        },
+                    ]}
+                />
+                <ExperienceSection
+                    header='Bit Project'
+                    experiences={[
+                        {
+                            title: 'Program Director',
+                            dates: 'February 2021 - July 2021',
+                            bulletPoints: [
                                 'Oversaw a 5-person team through the development of a 9 week-long undergraduate level data science curriculum about applying NumPy, Pandas, and Scikit-learn to digital humanities',
                                 'Directed a 5 week-long, 6-person focus group on the curriculum and converted collected feedback into deliverables',
-                            ]}
-                        />
-                    </div>
-                </div>
-                <div className='flex flex-col gap-y-4'>
-                    <div className='text-xl text-secondary'>New York University</div>
-                    <div className='flex flex-col gap-y-4'>
-                        <WorkExperience
-                            position='Learning Assistant'
-                            dates='January 2021 - December 2022'
-                            bulletPoints={[
+                            ],
+                        },
+                    ]}
+                />
+                <ExperienceSection
+                    header='New York University'
+                    experiences={[
+                        {
+                            title: 'Learning Assistant',
+                            dates: 'January 2021 - December 2022',
+                            bulletPoints: [
                                 'Instructed 260 students per semester in one-on-one and group sessions in algorithms, data structures, discrete mathematical logic, and linear algebra',
                                 'Developed internal tooling with a team of 4 in an Agile workflow and composed 12 pages of documentation on API usage and further development steps',
-                            ]}
-                        />
-                        <WorkExperience
-                            position='Grader'
-                            dates='August 2021 - December 2022'
-                            bulletPoints={[
+                            ],
+                        },
+                        {
+
+                            title: 'Grader',
+                            dates: 'August 2021 - December 2022',
+                            bulletPoints: [
                                 'Evaluated 110 assignments per week per semester with detailed feedback in calculus, mathematical logic, and statistical analysis',
                                 'Compiled monthly reports on student performance based on aggregated grading data and presented trends in understanding to professors and peers',
-                            ]}
-                        />
-                    </div>
-                </div>
+                            ],
+                        },
+                    ]}
+                />
+            </div>
+            {/*Education*/}
+            <div className='flex flex-col gap-y-6'>
+                <div className='text-2xl font-bold'>Education</div>
+                <ExperienceSection
+                    header='New York University'
+                    experiences={[
+                        {
+                            title: 'BA in Math and Computer Science',
+                            dates: 'August 2019 - December 2022',
+                            bulletPoints: [],
+                        },
+                    ]}
+                />
             </div>
         </div>
     );
