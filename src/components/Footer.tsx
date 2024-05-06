2625d4201384356d890e2d4cf4b4ffcfdbc4be84
b-x-wu <bridgette.x.wu@gmail.com>
2024-05-06 10:30:09 -0700

    fix: fix mobile vertical spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { HEART_SVG_ELEMENT } from './constants';
 
 const Footer: React.FC = () => {
     return (
-        <div className='mt-4 flex w-full flex-col content-center gap-y-3 p-6 text-sm text-disabled'>
+        <div className='mt-4 flex w-full flex-none flex-col content-center gap-y-3 p-6 text-sm text-disabled'>
             <div className='mx-auto w-fit'>---</div>
             <div className='mx-auto flex w-fit flex-row gap-x-3 align-middle'>
                 <div>made with</div>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,9 @@ import Footer from './Footer';
 const Layout: React.FC = () => {
     return (
         <div className='flex items-center justify-center'>
-            <div className='relative w-160 max-w-4/5'>
+            <div className='relative flex min-h-svh w-160 max-w-4/5 flex-col'>
                 <Header />
-                <div className='mb-auto min-h-body'>
+                <div className='mb-auto grow'>
                     <Outlet />
                 </div>
                 <Footer />
