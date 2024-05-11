85e51f530fe3de3a6a081b173777494a6a3ba62a
b-x-wu <bridgette.x.wu@gmail.com>
2024-05-11 03:44:52 -0700

    feat: add pixel grid

---
+++ src/components/projects/pixel-svg-maker/PixelCanvas.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+interface PixelCanvasProps {
+    /**
+     * the number of pixels wide the grid view is
+    */
+    pixelWidth: number
+    /**
+     * the number of pixels high the grid view is
+    */
+    pixelHeight: number
+}
+
+const PixelCanvas: React.FC<PixelCanvasProps> = ({
+    pixelWidth,
+    pixelHeight,
+}: PixelCanvasProps) => {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [ containerWidth, setContainerWidth ] = useState<number | undefined>(undefined);
+
+    const resizeObserver = new ResizeObserver((entries) => {
+        const resizedElement = entries.at(0);
+        if (resizedElement === undefined) {
+            return;
+        }
+
+        if (resizedElement.contentRect) {
+            setContainerWidth(resizedElement.contentRect.width);
+        }
+    });
+
+    const renderPixelGrid = (context: CanvasRenderingContext2D): void => {
+        if (containerWidth === undefined) {
+            return;
+        }
+
+        context.lineWidth = containerWidth / 1000;
+        const pixelSideLength = containerWidth / pixelWidth;
+        const scaledPixelSideLength = pixelSideLength * 0.92;
+        for (let x = 0; x < pixelWidth; x++) {
+            for (let y = 0; y < pixelHeight; y++) {
+                const centerX = (x + 0.5) * pixelSideLength;
+                const centerY = (y + 0.5) * pixelSideLength;
+                context.strokeRect(
+                    centerX - scaledPixelSideLength / 2,
+                    centerY - scaledPixelSideLength / 2,
+                    scaledPixelSideLength,
+                    scaledPixelSideLength,
+                );
+            }
+        }
+    };
+
+    // subscribe to resize observer
+    useEffect(() => {
+        const containerElement = containerRef.current;
+        if (containerElement === null) {
+            return;
+        }
+        resizeObserver.observe(containerElement);
+        return () => resizeObserver.unobserve(containerElement);
+    }, []);
+
+    // render grid and array when container size changes, or pixel dimensions change
+    useEffect(() => {
+        const canvasElement = canvasRef.current;
+        if (canvasElement === null || containerWidth === undefined) {
+            return;
+        }
+
+        // set canvas size
+        const aspectRatio = pixelHeight / pixelWidth;
+        const height = Math.round(containerWidth * aspectRatio);
+
+        canvasElement.width = containerWidth;
+        canvasElement.height = height;
+
+        const canvasContext = canvasElement.getContext('2d');
+        if (canvasContext === null) {
+            return;
+        }
+
+        renderPixelGrid(canvasContext);
+
+    }, [ pixelWidth, pixelHeight, containerWidth ]);
+
+    return (
+        <div ref={containerRef}>
+            <canvas ref={canvasRef}></canvas>
+        </div>
+    );
+};
+
+export default PixelCanvas;
+
---
+++ src/components/projects/pixel-svg-maker/PixelSvgMaker.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PixelCanvas from './PixelCanvas';
+
+const DEFAULT_PIXEL_WIDTH: number = 16;
+const DEFAULT_PIXEL_HEIGHT: number = 16;
+
+const PixelSvgMaker: React.FC = () => {
+    return (
+        <div>
+            <PixelCanvas pixelWidth={DEFAULT_PIXEL_WIDTH} pixelHeight={DEFAULT_PIXEL_HEIGHT} />
+        </div>
+    );
+};
+
+export default PixelSvgMaker;
+
