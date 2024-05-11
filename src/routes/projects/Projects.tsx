6c34df243c775667424bab5ac46f0a23ec0692c8
B Wu <wu.bruce.x@gmail.com>
2024-05-11 03:44:52 -0700

    feat: add pixel grid

--- src/routes/projects/Projects.tsx
+++ src/routes/projects/Projects.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // TODO: make this pretty. this is a placeholder for now
 const Projects: React.FC = () => {
     return (
         <div className='flex w-full flex-col gap-y-2'>
+            <Link to='/projects/pixel-svg-maker'>Pixel SVG Maker</Link>
         </div>
     );
 };
