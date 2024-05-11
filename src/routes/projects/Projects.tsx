3bf3f041a29a9b5f6ca07fe43ebe92b2edf22f46
B Wu <wu.bruce.x@gmail.com>
2024-05-10 22:12:51 -0700

    feat: add projects route

---
+++ src/routes/projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// TODO: make this pretty. this is a placeholder for now
+const Projects: React.FC = () => {
+    return (
+        <div className='flex w-full flex-col gap-y-2'>
+        </div>
+    );
+};
+
+export default Projects;
+
