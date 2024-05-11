0c40a5f97d2f407581f4cda645f5ffc152185bdf
b-x-wu <bridgette.x.wu@gmail.com>
2024-05-10 22:12:51 -0700

    feat: add projects route

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Error404 from './routes/Error404';
 import Home from './routes/Home';
 import Resume from './routes/Resume';
+import Projects from './routes/projects/Projects';
 
 const Router = (
     <BrowserRouter>
@@ -11,6 +12,9 @@ const Router = (
             <Route element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path='resume' element={<Resume />} />
+                <Route path='projects'>
+                    <Route index element={<Projects />} />
+                </Route>
                 <Route path='*' element={<Error404 />} />
             </Route>
         </Routes>
