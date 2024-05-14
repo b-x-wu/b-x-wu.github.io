e6c2bb36283c9908f508154767a1f4c19e3d1444
b-x-wu <bridgette.x.wu@gmail.com>
2024-05-14 00:55:53 -0700

    fix: remove slider from projects page

--- src/routes/projects/Projects.tsx
+++ src/routes/projects/Projects.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Slider from '../../components/common/Slider';
 
 // TODO: make this pretty. this is a placeholder for now
 const Projects: React.FC = () => {
     return (
         <div className='flex w-full flex-col gap-y-2'>
             <Link to='/projects/pixel-svg-maker'>Pixel SVG Maker</Link>
-            <Slider min={ 0 } max={ 100 } step={ 2 } onChange={ console.log } />
         </div>
     );
 };
