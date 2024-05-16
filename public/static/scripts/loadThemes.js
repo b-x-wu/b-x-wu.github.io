857a51db1abd3cf404c8f8a262efe5819c5940af
B Wu <wu.bruce.x@gmail.com>
2024-05-16 01:38:02 -0700

    feat: add user preference theme loading

---
+++ public/static/scripts/loadThemes.js
@@ -0,0 +1,37 @@
+const DARK_MODE_KEY = 'dark_mode';
+const HIGH_CONTRAST_MODE_KEY = 'high_contrast_mode';
+
+const setDarkMode = () => {
+    // check stored user preference
+    if (localStorage[DARK_MODE_KEY] === 'true') {
+        document.documentElement.classList.add('dark');
+        return;
+    }
+
+    // check prefers color scheme
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add('dark');
+        return;
+    }
+
+    // no preference. set light mode
+    document.documentElement.classList.remove('dark');
+};
+
+const setHighContrastMode = () => {
+    if (localStorage[HIGH_CONTRAST_MODE_KEY] === 'true') {
+        document.documentElement.classList.add('contrast');
+        return;
+    }
+
+    if (window.matchMedia('(prefers-contrast: mode)').matches) {
+        document.documentElement.classList.add('contrast');
+        return;
+    }
+
+    document.documentElement.classList.remove('contrast');
+};
+
+setDarkMode();
+setHighContrastMode();
+
