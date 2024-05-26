30009cefae0a7d2b4fc7d246fec69ef23bb08ddf
b-x-wu <bridgette.x.wu@gmail.com>
2024-05-26 01:47:48 -0700

    feat: add window preview for projects

--- src/routes/projects/Projects.tsx
+++ src/routes/projects/Projects.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import Chatbox from '../../components/home/Chatbox';
+import Window from '../../components/common/Window';
 
 // TODO: make this pretty. this is a placeholder for now
 const Projects: React.FC = () => {
+
+    const dateString = useMemo(() => (new Date()).toUTCString(), []);
+
     return (
-        <div className='flex w-full flex-col gap-y-2'>
-            <Link to='/projects/pixel-svg-maker'>Pixel SVG Maker</Link>
+        <div className='flex w-full flex-col space-y-4'>
+            <div className='flex h-9 w-full items-center justify-center text-center'>
+                <div className='text-disabled'>{ dateString }</div>
+            </div>
+            <Chatbox content={ <span className='text-secondary'>joined the room</span> } />
+            <Chatbox content={ 'here’s some stuff i\'ve worked on, check it out !!' } />
+            <Window
+                title='Pixel SVG Maker'
+                optionsBar='A web app for exporting pixel art to SVG'
+                controlButtons={ [
+                    <Link to='/projects/pixel-svg-maker' key='pixel-svg-link'>
+                        <div className="bg-text size-4 bg-clip-[url('/static/icons/link.svg')]" />
+                    </Link>,
+                ] }
+            >
+                <img src='/static/images/project-previews/pixel-svg-maker-preview.png' alt='Pixel SVG Maker preview' />
+            </Window>
         </div>
     );
 };
