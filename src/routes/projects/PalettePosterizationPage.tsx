6f98f01c4f37b9f9c5f65087ff70daa31ea47a90
B Wu <bxwu@amazon.com>
2024-08-28 17:57:06 -0700

    fix: fix palette spelling

--- src/routes/projects/PalettePosterizationPage.tsx
+++ src/routes/projects/PalettePosterizationPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PaletePosterization from '../../components/projects/palette-posterization/PalettePosterization';
+import PalettePosterization from '../../components/projects/palette-posterization/PalettePosterization';
 import { Link } from 'react-router-dom';
 import { useTitle } from '../../hooks';
 
@@ -8,7 +8,7 @@ const PalettePosterizationPage: React.FC = () => {
 
     return (
         <div className='flex w-full flex-col gap-y-8'>
-            <PaletePosterization />
+            <PalettePosterization />
             <div className='w-full text-center'>~~~</div>
             <div className='flex w-full flex-col gap-y-8'>
                 <h2 className='text-secondary text-2xl'>What is this?</h2>
