2354d99c472b86a34bc7b9536ff9f04250ade4ed
Bridgette Wu <bwu@patreon.com>
2026-04-05 17:30:11 -0700

    feat: settings buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.fontsource(),
+        name: "Ubuntu Mono",
+        cssVariable: "--font-ubuntu-mono",
+    }]
+});
---
+++ src/lib/utils.ts
@@ -0,0 +1,33 @@
+export type IconButtonProps =  { 'aria-label': string, src: string, 'data-tag'?: string };
+
+export const getByDataTag = (dataTag: string): HTMLElement => {
+    const eles = document.querySelectorAll(`[data-tag="${dataTag}"]`);
+    if (eles.length > 1) {
+        throw new Error(`Found ${eles.length} elements with data-tag: ${dataTag}`);
+    }
+
+    const ele =  eles.values().next().value;
+    if (ele === undefined) {
+        throw new Error(`Found no elements with data-tag: ${dataTag}`);
+    }
+
+    return ele as HTMLElement;
+}
+
+export const updateIconButton = (dataTag: string, { 'aria-label': ariaLabel, src }: Partial<IconButtonProps>): void => {
+    console.log({ ariaLabel, src })
+    const button = getByDataTag(dataTag);
+    const div = button.querySelector("div");
+
+    if (div === null) {
+        throw new Error();
+    }
+
+    if (src !== undefined) {
+        div.style.setProperty('--bg-image', `url(${src})`);
+    }
+
+    if (ariaLabel !== undefined) {
+        button.setAttribute("aria-label", ariaLabel);
+    }
+}
