2861fb225f0ed4b5efef00a9e8c981b0866a4c66
b-x-wu <bridgette.x.wu@gmail.com>
2026-04-05 17:30:11 -0700

    feat: settings buttons

---
+++ src/lib/store.ts
@@ -0,0 +1,50 @@
+import { persistentAtom } from "@nanostores/persistent";
+
+export const themeStore = persistentAtom<'light' | 'dark' | 'system'>('theme', 'system')
+export const contrastStore = persistentAtom<'default' | 'high'>('contrast', 'default');
+export const motionStore = persistentAtom<'default' | 'reduced'>('motion', 'default');
+
+export const initializeStores = () => {
+    themeStore.subscribe((value) => {
+        const isSystemDark = value === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (isSystemDark) {
+            document.documentElement.classList.add('dark');
+            return;
+        }
+
+        if (value === 'system') {
+            document.documentElement.classList.remove('dark');
+            return;
+        }
+
+        if (value === 'dark') {
+            document.documentElement.classList.add('dark');
+            return;
+        }
+        
+        // light mode
+        document.documentElement.classList.remove('dark');
+    })
+
+    contrastStore.subscribe((value) => {
+        if (value === 'high') {
+            document.documentElement.classList.add('contrast');
+        } else { // default contrast
+            document.documentElement.classList.remove('contrast');
+        }
+    })
+}
+
+export const getNormalizedTheme = (value?: 'light' | 'dark' | 'system') => {
+    const themeStoreValue = value ?? themeStore.get();
+    const isSystemDark = themeStoreValue === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (isSystemDark) {
+        return 'dark';
+    }
+
+    if (themeStoreValue === 'system') {
+        return 'light'
+    }
+
+    return themeStoreValue
+}
