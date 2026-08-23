365a226d69ac665120e4b0bf182ec4f2651faed8
b-x-wu <bridgette.x.wu@gmail.com>
2026-08-23 00:36:02 -0700

    style: refactor core components into directory

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,3 @@
-export type IconButtonProps = {
-  "aria-label": string;
-  src: string;
-  "data-tag"?: string;
-};
-
 export const getByDataTag = (dataTag: string): HTMLElement => {
   const eles = document.querySelectorAll(`[data-tag="${dataTag}"]`);
   if (eles.length > 1) {
@@ -16,24 +10,4 @@ export const getByDataTag = (dataTag: string): HTMLElement => {
   }
 
   return ele as HTMLElement;
-};
-
-export const updateIconButton = (
-  dataTag: string,
-  { "aria-label": ariaLabel, src }: Partial<IconButtonProps>,
-): void => {
-  const button = getByDataTag(dataTag);
-  const span = button.querySelector("span");
-
-  if (span === null) {
-    throw new Error();
-  }
-
-  if (src !== undefined) {
-    span.style.setProperty("--bg-image", `url(${src})`);
-  }
-
-  if (ariaLabel !== undefined) {
-    button.setAttribute("aria-label", ariaLabel);
-  }
 };
