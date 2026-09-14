fa341c01b86baa327dcefbf14a71fea637a6f241
bridgette wu <bridgette.x.wu@gmail.com>
2026-09-14 15:38:41 -0700

    feat: select posterizer options

--- src/components/pages/projects/palettePosterizer/state.ts
+++ src/components/pages/projects/palettePosterizer/state.ts
@@ -1,17 +1,17 @@
 import { map } from "nanostores";
 import type { Color } from "~/lib/color";
-import { ColorMetricType, ColorReducerType } from "./utils";
+import { ColorMetric, ColorReducer } from "./utils";
 
 export interface State {
   image: HTMLImageElement | undefined;
   palette: Color[];
-  colorMetric: ColorMetricType;
-  colorReducer: ColorReducerType;
+  colorMetric: ColorMetric;
+  colorReducer: ColorReducer;
 }
 
 export const stateStore = map<State>({
   image: undefined,
   palette: [],
-  colorMetric: ColorMetricType.EUCLIDEAN_RGB,
-  colorReducer: ColorReducerType.PALETTE,
+  colorMetric: ColorMetric.EUCLIDEAN_RGB,
+  colorReducer: ColorReducer.PALETTE,
 });
